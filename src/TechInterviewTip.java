c96ebc90fdccb9017d73b0be4ac1470173f1d9a8
hellojohnwoo <49154920+hellojohnwoo@users.noreply.github.com>
2019-04-20 22:27:24 +0900

    TechInterviewTip 0420 algo-study

---
+++ src/TechInterviewTip.java
@@ -0,0 +1,49 @@
+import java.util.List;
+
+public class TechInterviewTip {
+    // 1. Materialize requirements
+    public List<String> findNamesFromMovieScript(String movieScript) {
+        /*
+            followup Question
+         */
+    }
+
+
+    // 2. Utility method
+    public List<String> permutateString(String input) {
+        /*
+            input = ab
+            output = [ab, Ab, aB, AB]
+            Character.toLowerCase(char c)
+            Character.toUpperCase(char c
+         */
+    }
+
+
+    // 3. Active communication
+    public void veryDifficultQuestion() {
+
+    }
+
+
+    // 4. Start with small things
+    public class Sudoku {
+        public Sudoku(char[][] board) {
+
+        }
+        public char[][] solveSudoko() {
+
+        }
+    }
+    public boolean isValid(char[][] board) {
+
+    }
+    /*
+        Validation testing & Back tracking
+     */
+
+
+
+    // 5. Confidence
+
+}

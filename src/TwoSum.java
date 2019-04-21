7a80c7fb6999d2832895c06a0f8738a41a02375d
hellojohnwoo <49154920+hellojohnwoo@users.noreply.github.com>
2019-04-21 22:23:38 +0900

    TwoSum 0421 algo-study

---
+++ src/TwoSum.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0; i<nums.length; i++) {
+
+            int cur = nums[i];
+
+            if(map.containsKey(target-cur)) {
+                int[] ret = new int[2];
+                ret[0] = map.get(target-cur);
+                ret[1] = i;
+                return ret;
+            } else {
+                map.put(cur, i);
+            }
+        }
+        return null;
+    }
+}
